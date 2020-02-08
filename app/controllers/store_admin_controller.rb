134e8cac76f887674ee52146bc99141dc34e29c2
Hanna Lee <mani21na@gmail.com>
2020-02-08 00:05:51 +0000

    Automatically backed up by Learn

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,11 +1,15 @@
 class StoreAdminController < ApplicationController
-  layout "admin"
+  layout 'admin'
+
+  def home
+  end
 
   def orders
-    render :layout => 'order_administration'
+    render :layout => "order_administration"
   end
 
   def invoice
     render :layout => false
   end
+
 end
